1e1c04cd2e7a684348688a4a9aeb23114534b564
lukasdannemann <lukasdannemann@outlook.com>
2025-12-08 12:15:39 +0100

    removed node_modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router'
+import Home from './pages/Home'
+import TodosActivities from './pages/TodosActivities'
+import Habits from './pages/Habits'
+import EventPlanner from './pages/EventPlanner'
+import Login from './pages/Login'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/todos' element={<TodosActivities />}/>
+      <Route path='/habits' element={<Habits />}/>
+      <Route path='/events' element={<EventPlanner />}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='*' element={<ErrorPage />}/>
+    </Routes>
   )
 }
 
