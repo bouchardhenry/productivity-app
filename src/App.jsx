dd0168770ea064b45aa92424dc4b00ce99873cfd
emmywennersten <wennerstenemmy@gmail.com>
2025-12-09 13:01:26 +0100

    Added login and signup page + shared CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import TodosActivities from './pages/TodosActivities'
 import Habits from './pages/Habits'
 import EventPlanner from './pages/EventPlanner'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 import ErrorPage from './pages/ErrorPage'
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Login />}/>
+      <Route path='/signup' element={<SignUp />}/>
       <Route path='/todos' element={<TodosActivities />}/>
       <Route path='/habits' element={<Habits />}/>
       <Route path='/events' element={<EventPlanner />}/>
-      <Route path='/login' element={<Login />}/>
+      <Route path='/dashboard' element={<Home />}/>
       <Route path='*' element={<ErrorPage />}/>
     </Routes>
   )
