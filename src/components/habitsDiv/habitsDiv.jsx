cb4f6a9b8fe7fa79304403dba66859c89d6362c5
bouchardhenry <bouchardhenry@gmail.com>
2025-12-12 12:54:36 +0100

    added repetition function on habits

--- src/components/habitsDiv/habitsDiv.jsx
+++ src/components/habitsDiv/habitsDiv.jsx
@@ -1,22 +1,27 @@
 import { useContext } from "react";
 import { HabitsContext } from "../../context/HabitsContext";
+import styles from "./habitsDiv.module.css"
+import Repetition from "./repetiton";
 
 export default function HabitsDiv() {
   const { habits } = useContext(HabitsContext);
 
   return (
-    <div>
+    <div className={styles.habits}>
       <h2>Your Habits</h2>
+      <div className={styles.habitCards}>
       {habits.length === 0 ? (
         <p>No habits yet. Add one to get started!</p>
       ) : (
         habits.map((habit) => (
-          <div key={habit.id}>
+          <div key={habit.id} className={styles.habitCard}>
             <h3>{habit.habit}</h3>
             <p>Priority: {habit.priority}</p>
+            <Repetition />
           </div>
         ))
       )}
+      </div>
     </div>
   );
 }
