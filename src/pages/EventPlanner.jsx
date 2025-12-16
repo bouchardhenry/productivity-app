522a33211b211f773419b3d3a8bd6833bd3b5399
bouchardhenry <bouchardhenry@gmail.com>
2025-12-16 10:04:00 +0100

    hide/show navbar on respective pages

--- src/pages/EventPlanner.jsx
+++ src/pages/EventPlanner.jsx
@@ -1,17 +1,30 @@
-import EventForm from '../components/eventForm/EventForm'
-import Events from '../components/EventsDiv/Events'
-import { useState } from 'react'
-import styles from '../components/eventForm/EventForm.module.css'
+import EventForm from "../components/eventForm/EventForm";
+import Events from "../components/EventsDiv/Events";
+import { useState } from "react";
+import styles from "../components/eventForm/EventForm.module.css";
+import Navigation from "../components/navigation/Navigation";
 
-export default function EventPlanner(){
+export default function EventPlanner() {
+  const [show, setShow] = useState(true);
 
-    const [show, setShow] = useState(true)
-
-    return(
-        <>
-        {!show ? <EventForm /> : 
-        <button onClick={() => setShow(false)} className={styles.newEventButton}>Skapa ny händelse</button>}
-        <Events />
-        </>
-    )
+  return (
+    <>
+      <div className="layout">
+        <Navigation />
+        <main className="content">
+          {!show ? (
+            <EventForm />
+          ) : (
+            <button
+              onClick={() => setShow(false)}
+              className={styles.newEventButton}
+            >
+              Skapa ny händelse
+            </button>
+          )}
+          <Events />
+        </main>
+      </div>
+    </>
+  );
 }
